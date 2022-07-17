dacf7b1fc6f0604d9b81dc7137812c8298f31ed5
KagariSoft-Dev <dev@kagarisoft.com>
2022-07-17 19:05:29 -0300

    update button.js

--- react/components/button.js
+++ react/components/button.js
@@ -1,5 +1,10 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Button;
+
 require("core-js/modules/es.object.assign.js");
 
 function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
