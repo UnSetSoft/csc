6bcf66cfae685a9da561daacb1fa808969f22fae
KagariSoft-Dev <dev@kagarisoft.com>
2022-08-30 23:31:16 -0300

    update package.json, package.json, main.js and common.scss

--- packages/csc/src/js/main.js
+++ packages/csc/src/js/main.js
@@ -2,7 +2,7 @@
 * CSC (Cascading style component)
 * Author: Jorge Maxiliano Toledo
 * Type: SCSS/CSS Framework
-* Version: 1.3.1
+* Version: 1.3.2
 * GitHub: https://github.com/KagariSoft/csc
 */
 
