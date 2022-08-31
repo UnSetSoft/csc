99fa942fcd3a4edc01d5cbf551aa3a728634f320
KagariSoft-Dev <dev@kagarisoft.com>
2022-08-30 23:53:54 -0300

    update package.json, package.json, main.js and common.scss

--- packages/csc/src/js/main.js
+++ packages/csc/src/js/main.js
@@ -1,10 +1,10 @@
 /*!
-* CSC (Cascading style component)
-* Author: Jorge Maxiliano Toledo
-* Type: SCSS/CSS Framework
-* Version: 1.3.3
-* GitHub: https://github.com/KagariSoft/csc
-*/
+ * CSC (Cascading style component)
+ * Author: Jorge Maxiliano Toledo
+ * Type: SCSS/CSS Framework
+ * Version: 1.3.4
+ * GitHub: https://github.com/KagariSoft/csc
+ */
 
 document
   .querySelector("[data-togglemenu]")
