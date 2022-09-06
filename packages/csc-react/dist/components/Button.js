ac7ccbd974c5492d60c259c9102c2030e14cbeee
KagariSoft-dev <runner@fv-az183-364.aixeropmy22uphdfpsuanqabdh.cx.internal.cloudapp.net>
2022-09-06 23:54:24 +0000

    🏗️ Files updated

--- packages/csc-react/dist/components/Button.js
+++ packages/csc-react/dist/components/Button.js
@@ -10,8 +10,14 @@ var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const Button = props => {
+  const styleArg = props.styleArg ? "kg-".concat(props.styleArg) : '';
+  const width = props.width ? "kg-".concat(props.width) : '';
+  const customClass = props.customClass ? props.customClass : '';
+  const onClickButton = props.onClickButton ? props.onClickButton : null;
+  console.log(customClass);
   return /*#__PURE__*/_react.default.createElement("button", {
-    className: props.styleArg ? "kg__button ".concat(props.styleArg) : 'kg__button'
+    className: ["kg__button ".concat(styleArg).concat(width), customClass].join(' '),
+    onClick: onClickButton
   }, props.label);
 };
 
