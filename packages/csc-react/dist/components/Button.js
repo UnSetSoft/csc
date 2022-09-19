40b0135e918b8375e4b14f2e68231eb9d7de4159
KagariSoft-dev <runner@fv-az198-602.gjkxsqtjg41etoza45cw35jkyb.bx.internal.cloudapp.net>
2022-09-19 15:50:41 +0000

    🏗️ Files updated

--- packages/csc-react/dist/components/Button.js
+++ packages/csc-react/dist/components/Button.js
@@ -7,6 +7,8 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+var _reactFontawesome = require("@fortawesome/react-fontawesome");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const Button = props => {
@@ -14,10 +16,16 @@ const Button = props => {
   const width = props.width ? "kg-".concat(props.width) : '';
   const customClass = props.customClass ? props.customClass : '';
   const onClickButton = props.onClickButton ? props.onClickButton : null;
-  return /*#__PURE__*/_react.default.createElement("button", {
+  const icon = props.icon ? props.icon : null;
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, icon ? /*#__PURE__*/_react.default.createElement("button", {
     className: ["kg__button ".concat(styleArg, " ").concat(width), customClass].join(' '),
     onClick: onClickButton
-  }, props.label);
+  }, /*#__PURE__*/_react.default.createElement("span", null, /*#__PURE__*/_react.default.createElement(_reactFontawesome.FontAwesomeIcon, {
+    icon: icon
+  }), " ", props.label)) : /*#__PURE__*/_react.default.createElement("button", {
+    className: ["kg__button ".concat(styleArg, " ").concat(width), customClass].join(' '),
+    onClick: onClickButton
+  }, /*#__PURE__*/_react.default.createElement("span", null, props.label)));
 };
 
 var _default = Button;
