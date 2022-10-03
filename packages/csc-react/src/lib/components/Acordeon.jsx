55d5882fc53daceaedeeebc169710e2c22119397
KagariSoft-Dev <dev@kagarisoft.com>
2022-10-03 02:05:42 -0300

    update 4 files and delete 1 file

---
+++ packages/csc-react/src/lib/components/Acordeon.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSortDown } from '@fortawesome/free-solid-svg-icons'
+const Acordeon = ({ children, title, icon, iconColor, className }) => {
+      const [open, setOpen] = useState(false)
+
+      useEffect(() => {
+
+      }, [iconColor, icon])
+
+      const i = icon ? icon : null;
+
+      const style = {
+            color: `#${iconColor}` || '#3f3f3f'
+      }
+
+      const OpenAcordeon = () => {
+            setOpen(prevCheck => !prevCheck);
+      }
+      return (
+            <div className={className ? `kg__acordeon ${className}` : "kg__acordeon"} onClick={OpenAcordeon}>
+                  <div className='kg__acordeon__title'>
+                        <span>{icon && <FontAwesomeIcon style={style} icon={i} />} {title}</span>
+                        <div className={open ? `kg__acordeon__title__icon kg__acordeon__title__icon__open` : `kg__acordeon__title__icon kg__acordeon__title__icon__default`}><FontAwesomeIcon icon={faSortDown} /></div>
+                  </div>
+                  {open ? (
+                        <div className='kg__acordeon__body'>
+                              <div className='kg__acordeon__body__container'>
+                                    {children}
+                              </div>
+                        </div>
+                  ) : null}
+            </div>
+      )
+}
+
+export default Acordeon;
--- packages/csc-react/src/lib/index.jsx
+++ packages/csc-react/src/lib/index.jsx
@@ -1,12 +1,14 @@
 import '@kagarisoft/csc/dist/css/common.min.css' // import the css from the other package.
 import Button from './components/Button';
-import Details from './components/details'
+import Acordeon from './components/Acordeon'
 import Alert from './components/Alerts';
 import Image from './components/Image';
+import Container from './Layout/Container';
 export {
       Button,
-      Details,
+      Acordeon,
       Alert,
-      Image
+      Image,
+      Container
 }
 
