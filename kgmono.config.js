7511e05c864634a7c772bd9229e01d42d1f35ce8
KagariSoft-Dev <dev@kagarisoft.com>
2022-10-03 17:57:57 -0300

    build: update package.json, package.json and package.json

---
+++ kgmono.config.js
@@ -0,0 +1,12 @@
+/*
+Include a list of package.json, for example:
+* Important, don't add "../../../"
+[
+      "packages/test/package.json",
+      "packages/test2/package.json"
+]
+*/
+module.exports = [
+  "packages/csc/package.json",
+  "packages/csc-react/package.json",
+];
